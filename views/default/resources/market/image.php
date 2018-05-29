7a72ee29bba408cce75e2d5b93717f5ef9219bde
chvibert <chvibert@yahoo.fr>
2018-05-29 17:06:04 +0200

    Fix the default image problem on the creative form

--- views/default/resources/market/image.php
+++ views/default/resources/market/image.php
@@ -12,50 +12,54 @@ $guid = elgg_extract('guid', $vars);
 
 $entity = get_entity($guid);
 
-if (!elgg_instanceof($entity, 'object', 'market')) {
-	return;
+if (!elgg_instanceof($entity, 'object', 'market') && $guid != -1) {
+    return;
 }
 
 // Get the size
 $sizenum = elgg_extract('size', $vars);
 $size = strtolower($sizenum);
-if (!in_array($size,array('large','medium','small','tiny','master'))) {
-	$size = "medium";
+if (!in_array($size, array('large', 'medium', 'small', 'tiny', 'master'))) {
+    $size = "medium";
 }
 
 // Get image number (1,2,3 or 4)
 $imagenum = elgg_extract('imagenum', $vars);
 if ($imagenum == 1) {
-	$imagenum = '';
+    $imagenum = '';
 }
+
+
+$success = false;
+if (elgg_instanceof($entity, 'object', 'market')) {
 
 // Try and get the icon
-$filehandler = new ElggFile();
-$filehandler->owner_guid = $entity->owner_guid;
-$filehandler->setFilename("market/" . $entity->guid . $size . $imagenum . ".jpg");
-		
-$success = false;
-if ($filehandler->open("read")) {
-	if ($contents = $filehandler->read($filehandler->getSize())) {
-		$success = true;
-	} 
-}
+    $filehandler = new ElggFile();
+    $filehandler->owner_guid = $entity->owner_guid;
+    $filehandler->setFilename("market/" . $entity->guid . $size . $imagenum . ".jpg");
 
+
+    if ($filehandler->open("read")) {
+        if ($contents = $filehandler->read($filehandler->getSize())) {
+            $success = true;
+        }
+    }
+}
 if (!$success) {
-	$path = elgg_get_site_url() . "mod/market/graphics/noimage{$size}.png";
-	header("Location: $path");
-	exit;
+    $path = elgg_get_site_url() . "mod/market/graphics/noimage{$size}.png";
+    header("Location: $path");
+    exit;
 }
 
 header("Content-type: image/jpeg");
-header('Expires: ' . date('r',time() + 864000));
+header('Expires: ' . date('r', time() + 864000));
 header("Pragma: public");
 header("Cache-Control: public");
 header("Content-Length: " . strlen($contents));
 
 $splitString = str_split($contents, 1024);
 
-foreach($splitString as $chunk) {
-	echo $chunk;
+foreach ($splitString as $chunk) {
+    echo $chunk;
 }
 
